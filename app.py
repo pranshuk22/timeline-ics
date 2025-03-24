0cf1c2b1f89e2ff55f631a74d0bf55acf1852b79
pranshuk22 <130257873+pranshuk22@users.noreply.github.com>
2025-03-24 10:39:44 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -90,5 +90,7 @@ def get_google_doc_html():
 def get_doc():
     return jsonify({"content": get_google_doc_html()})
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    from os import environ
+    app.run(host="0.0.0.0", port=int(environ.get("PORT", 5000)))
+
