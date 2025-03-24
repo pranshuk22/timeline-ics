cbddb992cf05e7529ec11e163208a73a730a8f45
pranshuk22 <130257873+pranshuk22@users.noreply.github.com>
2025-03-24 11:30:41 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -97,5 +97,5 @@ def get_doc():
     return jsonify({"content": get_google_doc_html()})
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
+    port = int(os.environ.get("PORT", 10000))
     app.run(host="0.0.0.0", port=port)
